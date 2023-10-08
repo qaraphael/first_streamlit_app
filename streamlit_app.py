d14344503f5b4b17e5d95b74946d37cf65d22987
qaraphael <147287082+qaraphael@users.noreply.github.com>
2023-10-08 15:48:20 +1300

    Test commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.title('My Parent\'s New Healthy Diner QQ')
+streamlit.title('My Parent\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
