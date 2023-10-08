d43d02ebb0dd9078cbbc69ae3b70bd07a98bf7e0
Raphael QA <qaraphael@gmail.com>
2023-10-08 11:27:12 +0000

    fix function def

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 # ----------------------------- #
 # create function to get fruit data
- def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice)
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
@@ -40,7 +40,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    fruit_choice = streamlit.text_input('What fruit would you like information about now?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information")
     else:
