0b87cbf684643ced08f70ca3f7faf3e8ca48ad95
qaraphael <147287082+qaraphael@users.noreply.github.com>
2023-10-08 14:20:40 +1300

    Updated Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit
 streamlit.title('My Parent\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3')
-streamlit.text('Kale, Spinach, Rocket Smoothie')
-streamlit.text('Hard-boiled Free-range Egg')
+streamlit.text('🥣 Omega 3')
+streamlit.text('🥗 Kale, Spinach, Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-range Egg')
+streamlit.text('🥑🍞 Avocando Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
