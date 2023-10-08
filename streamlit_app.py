490a603dca6a53882df767e19bd53f4ebda81d8b
Raphael QA <qaraphael@gmail.com>
2023-10-08 09:05:02 +0000

    add prompt for fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,14 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?', fruit_choice)
+streamlit.write('The user entered ', fruit_choice)
+
 
 import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response)
 #streamlit.text(fruityvice_response.json())
 
