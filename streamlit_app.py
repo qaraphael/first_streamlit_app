8952cac52b1c02ba870102ef43abe036ddb9551a
qaraphael <147287082+qaraphael@users.noreply.github.com>
2023-10-08 14:17:19 +1300

    Add menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parent\'s New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3')
+streamlit.text('Kale, Spinach, Rocket Smoothie')
+streamlit.text('Hard-boiled Free-range Egg')
