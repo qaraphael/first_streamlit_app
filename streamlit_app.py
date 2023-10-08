90e3b563b797f5c6d66a8f90d572c15cd360e0ba
Raphael QA <qaraphael@gmail.com>
2023-10-08 09:00:27 +0000

    change to kiwi instead of watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-streamlit.text(fruityvice_response.json())
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response)
+#streamlit.text(fruityvice_response.json())
 
 # Normalize semi-structured JSON data into a flat table.
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
